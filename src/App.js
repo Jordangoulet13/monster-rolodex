88c852bd7b65eaac32dfc75b61fae68e649812bb
unknown <jordangoulet13@gmail.com>
2021-01-22 23:34:08 -0800

    add card and images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        { name: "Frankeinstein" },
-        { name: "Dracula" },
-        { name: "Zombie" },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster, index) => (
-          <h1 key={index}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
