1055582327183eaf75eb5c2a54e21e33bc333edc
unknown <jordangoulet13@gmail.com>
2021-01-26 19:01:22 -0800

    add styling to title and body

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-import "./App.styles.css";
+import "./App.css";
 
 import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
@@ -21,6 +20,10 @@ class App extends React.Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
     const { monsters, searchField } = this.state; //destructure
     const filteredMonsters = monsters.filter((monster) =>
@@ -28,9 +31,10 @@ class App extends React.Component {
     );
     return (
       <div className="App">
+        <h1>Monster Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
